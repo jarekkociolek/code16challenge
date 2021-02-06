07f35310e26852ea9361facb6273e0f0caf56ecf
jarekkociolek <jarekkociolek@gmail.com>
2021-02-06 20:15:26 +0100

    launch web browser with a charity link

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+        bool isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        bool isMacOs = RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+        var tossACoinToMaja = "https://www.siepomaga.pl/skarbonki/code16challenge/koszyk/dodaj";
+        if (isWindows)
+            Process.Start(new ProcessStartInfo { FileName = tossACoinToMaja, UseShellExecute = true });
+        if (isLinux) Process.Start("open", tossACoinToMaja);
+        if (isMacOs) Process.Start("open", tossACoinToMaja);
+    }
+}
